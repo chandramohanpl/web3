7f04b1d6bde71b2f89082a3f243bf857f66e25ae
Chandra <chandramohan.padmachetty@gmail.com>
2023-02-06 19:42:31 +0530

    smart_contract_lottery

---
+++ hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
@@ -0,0 +1,77 @@
+const { network, ethers } = require("hardhat")
+const {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const VRF_FUND_AMOUNT = ethers.utils.parseEther("30") // 1 Ether, or 1e18 (10^18) Wei
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+
+    if (developmentChains.includes(network.name)) {
+        // create VRFV2 Subscription
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription. Usually we should LINK token on a real network
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+
+    // const arguments = [
+    //     vrfCoordinatorV2Address,
+    //     subscriptionId,
+    //     networkConfig[chainId]["gasLane"],
+    //     networkConfig[chainId]["keepersUpdateInterval"],
+    //     networkConfig[chainId]["raffleEntranceFee"],
+    //     networkConfig[chainId]["callbackGasLimit"],
+    // ]
+    const arguments = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: arguments,
+        logs: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Ensure the Raffle contract is a valid consumer of the VRFCoordinatorV2Mock contract.
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+    }
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_APIKEY) {
+        log("Verifying... ")
+        await verify(raffle.address, arguments)
+    }
+    log("-----------------------------------------------------------")
+}
+
+module.exports.tags = ["all", "raffle"]
---
+++ hardhat-smartcontract-lottery-fcc/hardhat.config.js
@@ -0,0 +1,76 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-goerli.g.alchemy.com/v2/BNE5Pxjh-pFvTDM1Ve1kKJwLkwOd6DBl"
+const ETHERSCAN_APIKEY = process.env.ETHERSCAN_APIKEY
+const COINMARKETCAP_APIKEY = process.env.COINMARKETCAP_APIKEY
+const REPORT_GAS = process.env.REPORT_GAS || false
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_APIKEY,
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Raffle"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: "0.8.17",
+    mocha: {
+        timeout: 500000, // 500 seconds max for running tests
+    },
+}
